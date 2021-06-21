34e50a7bc881400c2d01938b5e9657f7fe761f94
Flavio Miguel <85520095+flavio-m-santos@users.noreply.github.com>
2021-06-21 09:12:27 -0300

    treinamento
    enviando para o git

---
+++ simula_dados.py
@@ -0,0 +1,13 @@
+import random
+opcao = 'Digite qualquer tecla para jogar o dado ou '
+opcao += 'digite S para sair: '
+
+escolha = ""
+while escolha != 's':
+    escolha = input(opcao)
+    lados = ['lado 1', 'lado 2', 'lado 3', 'lado 4', 'lado 5', 'lado 6']
+    sort = random.choice(lados)
+    print('O dado caiu em : ', sort)
+
+    '''if escolha != 'j':
+        print(opcao)'''
